7bb49396908d0dc3385560301377b90ad54ac555
Quentin Smith <quentin@mit.edu>
2020-03-16 00:18:08 -0400

    Port dsheen's code to run with an external flowgraph

---
+++ docker/radioastronomy/gal_scan/run.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+
+# Prepare for Python 3
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import argparse
+import time
+import survey_autoranging
+
+##################################################
+##################################################
+##################################################
+            # RCI STUFF #
+##################################################
+##################################################
+##################################################
+az_offset=5.5
+el_offset=-5.5
+import rci.client
+client = rci.client.Client()
+client.set_offsets(az_offset, el_offset)
+
+def point(az,el): #point the dish
+    client.set_azimuth_position(az)
+    client.set_elevation_position(el)
+    print('Moving to position ' + str(az)+' , '+str(el)+'.')
+    return
+
+def park():
+    point(250,50)
+    print("Parking")
+    return
+
+from flowgraph import flowgraph
+
+class top_block(flowgraph):
+    def __init__(self):
+        super(top_block, self).__init__()
+        self.darksky = None
+
+    def set_recording_enabled(self, enabled):
+        # TODO: Rename block to something more meaningful.
+        self.blocks_copy_0.set_enabled(enabled)
+
+    def snapshot(self, int_time): #straight snapshot over a certain integration time.
+        self.set_integration_time(int_time)
+        print('Snapshot %d sec' % (int_time,))
+        vec=self.get_variable_function_probe() #old vector
+        pointa=vec[0]
+        pointb=vec[-1]
+        self.set_recording_enabled(True) #start copying
+        while vec[0]==pointa and vec[-1]==pointb:
+            time.sleep(1)
+            vec=tb.get_variable_function_probe()
+        tb.set_recording_enabled(False) #stop copying
+        return np.array(vec)
+
+    def dark_sky_calib(self, int_time): #for use when pointing at the dark sky
+        self.darksky = self.snapshot(int_time)
+
+    def observe(self, int_time): #dark sky calbrated snapshot
+        vec=self.snapshot(int_time)
+        # TODO: Why did the original code wait for int_time here?
+        if self.darksky:
+            return vec-self.darksky
+        else:
+            print('Warning: No dark sky calibration has been performed.')
+            return vec
+        
+
+#axis extremes
+flo=1420.4-0.75
+fhi=1420.4+0.75
+def graphing(tb, int_time, iter=float('inf')):
+    plt.ion()
+    plt.figure()
+    vec=tb.get_variable_function_probe() #vector
+    n=len(vec)
+    x=np.linspace(flo, fhi, n)
+    i=0
+    while(i<iter):
+        plt.pause(int_time)
+        y=tb.observe(int_time)
+        plt.clf()
+        plt.xlabel('Frequency (MHz)')
+        plt.ylabel('Scaled power')
+        plt.axvline(x=1420.406, color='black', ls='--')
+        plt.ticklabel_format(useOffset=False)
+        plt.plot(x,y)
+        plt.draw()
+        i+=1
+
+
+def main(top_block_cls=top_block, options=None):
+    parser = argparse.ArgumentParser(description='Galactic sky scan')
+    parser.add_argument('output_dir', metavar='DIRECTORY',
+                        help='output directory to write scan results')
+    parser.add_argument('--int-time', type=int,
+                        help='integration time')
+    args = parser.parse_args()
+    
+    tb = top_block_cls()
+    tb.start()
+    print('Receiving ...')
+
+    survey_autoranging.run_survey(tb, point, savefolder=args.output_dir, int_time=args.int_time)
+
+    tb.stop()
+    tb.wait()
+
+if __name__ == '__main__':
+    main()
---
+++ docker/radioastronomy/gal_scan/survey_autoranging.py
@@ -0,0 +1,116 @@
+# Prepare for Python 3
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from galcoord import gal_to_altaz
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from galcoord import get_time
+import os.path
+import time
+
+#convert frequency f to radial velocity at galactic coordinate l
+#account for movement of the sun relative to galactic center
+def freq_to_vel(f,l):
+    c=2.998e5 #km/s
+    v_rec=(freq-f)*c/freq
+    v_sun=220 #km/s
+    correction=v_sun*np.sin(np.deg2rad(l))
+    return v_rec+correction
+
+def run_survey(tb, point, savefolder, savetitle='vectors.txt', int_time=30, l_start=0, l_stop=360, l_step=2.5)
+    gain=60
+    tb.set_sdr_gain(gain)
+    freq=tb.get_sdr_frequency()/1000000 #MHz
+    freq_offset=tb.get_output_vector_bandwidth()/2000000. #MHz
+    flo= freq-freq_offset
+    fhi= freq+freq_offset
+    num_chan=tb.get_num_channels()
+    freq_range=np.linspace(flo, fhi, num_chan)
+
+    #########################################
+    # BEGIN COMMANDS #
+    #########################################
+    large_step=1 #signals that the dish just moved a bunch and we need to wait longer for it to settle
+
+    #point at galactic center and give time to settle
+    l=l_start
+    #print('Moving to initial galactic longitude l=%f' % (l,))
+    # pos=gal_to_altaz(l,0)
+    # # while pos[1] <= 0:
+    # #     pos=gal_to_altaz(l,0)
+    # #     if pos[1] > 0: break
+    # #     if l > l_stop: break
+    # point(pos[0],pos[1])
+    # wait(10)
+
+    #do the survey
+    file=open(os.path.join(savefolder, savetitle), 'w')
+    file.write('Integration time '+str(int_time)+' seconds. Center frequency '+str(freq)+' MHz. \n \n')
+    file.write('Azimuth Elevation RA DEC Time Center Data_vector \n \n')
+    while l<=l_stop:
+        #take data at a position
+        pos=gal_to_altaz(l,0)
+        if pos[1] > 0:
+            print 'Moving to galactic longitude l=' + str(l)
+            point(pos[0],pos[1])
+            if large_step:
+                time.sleep(10)
+            else:
+                time.sleep(2)
+            large_step=0
+
+            print "Observing at galactic coordinates ("+str(l)+', 0).'
+            time=get_time()
+            data=observe(int_time) 
+
+            #write to file
+            file.write(str(l)+' ')
+            file.write(str(0)+' ')
+            file.write(str(time)+' ')
+            file.write(str(data)+'\n \n')
+
+            #frequency binned figure
+            plt.figure()
+            plt.title('l='+str(l)+ ' '+ str(time))
+            plt.xlabel('Frequency (MHz)')
+            plt.ylabel('Power at Feed (W/Hz)')
+            plt.axvline(x=freq, color='black', ls='--')
+            plt.ticklabel_format(useOffset=False)
+            plt.plot(freq_range, data)
+            plt.savefig(os.path.join(savefolder, 'lat'+str(l)+'_freq.pdf'))
+            time.sleep(1)
+            plt.close()
+
+            #velocity binned figure
+            vel_range=np.array([freq_to_vel(f,l) for f in freq_range])
+            center_vel=freq_to_vel(freq,l)
+
+            plt.figure()
+            plt.title('l='+str(l)+ ' '+ str(time))
+            plt.xlabel('Velocity (km/s)')
+            plt.ylabel('Power at Feed (W)')
+            plt.axvline(x=0, color='black', ls='--')
+            plt.ticklabel_format(useOffset=False)
+            plt.plot(vel_range, data)
+            plt.savefig(savefolder+'lat'+str(l)+'_vel.pdf')
+            time.sleep(1)
+            plt.close()
+
+            print('Data logged.')
+            print()
+
+        else:
+            large_step=1
+
+        l+=l_step
+        if l >= l_stop: break
+
+        #move to next position
+        # pos=gal_to_altaz(l,0)
+        # point(pos[0],pos[1])
+        # wait(2)
+
+    file.close()
