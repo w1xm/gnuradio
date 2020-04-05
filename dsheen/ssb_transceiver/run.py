dcdcab360f1b86bc648e3c2a17c330e9007e011d
Quentin Smith <quentin@mit.edu>
2020-04-05 04:40:46 -0400

    Update ssb_transceiver for GNU Radio 3.8 and PyQt5

--- dsheen/ssb_transceiver/run.py
+++ dsheen/ssb_transceiver/run.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python2
 import sys
+import signal
 import logging
 import threading
 import top_block
 import rigctld
-from PyQt4 import Qt, QtCore
+from rci import client
+from PyQt5 import Qt, QtCore
 from gnuradio import gr
 
 class Signal(QtCore.QObject):
@@ -14,22 +16,50 @@ class Signal(QtCore.QObject):
     set_volume = QtCore.pyqtSignal(float)
     set_mic_gain = QtCore.pyqtSignal(float)
 
+class top_block(top_block.top_block):
+    def __init__(self):
+        super(top_block, self).__init__()
+        self._client = client.Client()
+
+    def set_ptt_command(self, ptt):
+        with self._lock:
+            if ptt != self.get_ptt_command():
+                super(top_block, self).set_ptt_command(ptt)
+                if ptt:
+                    try:
+                        self._client.set_band_tx(0, True)
+                    except IOError as e:
+                        logging.exception("failed to enable tx")
+                    else:
+                        self.set_ptt_allowed(1)
+                else:
+                    self.set_ptt_allowed(0)
+                    self._client.set_band_rx(0, True)
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.WARNING, format="%(asctime)-15s %(levelname)s %(message)s")
     if gr.enable_realtime_scheduling() != gr.RT_OK:
         print "Error: failed to enable real-time scheduling."
 
     from distutils.version import StrictVersion
-    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
         Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
     if "-v" in sys.argv:
         logging.getLogger().setLevel(logging.INFO)
 
-    tb = top_block.top_block()
+    tb = top_block()
     tb.start()
     tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
     signal = Signal()
     signal.set_RF_frequency.connect(tb.set_RF_frequency, type=QtCore.Qt.BlockingQueuedConnection)
     signal.set_ptt_command.connect(tb.set_ptt_command, type=QtCore.Qt.BlockingQueuedConnection)
@@ -45,5 +75,5 @@ if __name__ == '__main__':
         s.shutdown()
         tb.stop()
         tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.aboutToQuit.connect(quitting)
     qapp.exec_()
