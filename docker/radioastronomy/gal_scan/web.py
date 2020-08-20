e6967ce4e87ffd300cdb47b02817257da28d96ee
Quentin Smith <quentin@mit.edu>
2020-08-20 05:59:37 -0400

    Restructure Bokeh server as an executable script and allow multiple sessions

---
+++ docker/radioastronomy/gal_scan/web.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+from bokeh.application.application import Application
+from bokeh.application.handlers.handler import Handler
+from bokeh.server.server import Server
+from bokeh.plotting import curdoc
+from gnuradio import gr, blocks
+import bokehgui
+import numpy as np
+import socket
+import rci.client
+import run
+
+class SessionHandler(Handler):
+    def __init__(self):
+        super().__init__()
+        self.sinks = {}
+
+    def on_server_loaded(self, server_context):
+        self.client = rci.client.Client(client_name='gal_scan')
+        self.client.set_offsets(run.az_offset, run.el_offset)
+
+        self.tb = run.radiotelescope(client=self.client)
+        self.mag_to_aW = blocks.multiply_const_vff([1e18] * self.tb.get_num_channels())
+        self.tb.connect((self.tb.blocks_multiply_const_vxx_0, 0), (self.mag_to_aW, 0))
+        null_sink = blocks.null_sink(gr.sizeof_float*self.tb.get_num_channels())
+        self.tb.connect(self.mag_to_aW, null_sink)
+
+        self.tb.start()
+
+    def on_server_unloaded(self, server_context):
+        self.tb.stop()
+        self.tb.wait()
+
+    async def on_session_created(self, session_context):
+        self.tb.lock()
+        self.sinks[session_context.id] = bokehgui.vec_sink_f_proc(self.tb.get_num_channels(), "", 1)
+        self.tb.connect((self.mag_to_aW, 0), (self.sinks[session_context.id], 0))
+        self.tb.unlock()
+
+    async def on_session_destroyed(self, session_context):
+        self.tb.lock()
+        self.tb.disconnect((self.mag_to_aW, 0), (self.sinks[session_context.id], 0))
+        del self.sinks[session_context.id]
+        self.tb.unlock()
+
+    def modify_document(self, doc):
+        doc.title = "Gal Scan GUI"
+
+        plot_lst = []
+
+        plot = bokehgui.vec_sink_f(doc, plot_lst, self.sinks[doc.session_context.id], is_message =False)
+
+        plot.initialize(update_time = 100, legend_list = [''])
+        plot.set_y_axis([0, 10])
+        plot.set_y_label("Power at feed (aW / Hz)")
+        plot.set_x_label("Frequency (MHz)")
+        plot.set_x_values(np.linspace(self.tb.get_sdr_frequency()-(self.tb.get_output_vector_bandwidth()/2), self.tb.get_sdr_frequency()+(self.tb.get_output_vector_bandwidth()/2), self.tb.get_num_channels())/1e6)
+        plot.enable_axis_labels(True)
+        plot.set_layout(1,0)
+        plot.enable_max_hold()
+        plot.format_line(0, "blue", 1, "solid", None, 1.0)
+        layout_t = bokehgui.bokeh_layout.create_layout([plot], "fixed")
+        doc.add_root(layout_t)
+
+if __name__ == '__main__':
+    server = Server(
+        {'/': Application(SessionHandler())},
+        allow_websocket_origin=[socket.getfqdn().lower()+":5006"],
+    )
+    server.start()
+    server.io_loop.start()
