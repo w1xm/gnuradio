213af970ec19ca7c19710692c7efae8758e07b11
Quentin Smith <quentin@mit.edu>
2020-08-21 05:31:52 -0400

    Show recent log entries in web UI

--- docker/radioastronomy/gal_scan/web.py
+++ docker/radioastronomy/gal_scan/web.py
@@ -4,21 +4,54 @@ from bokeh.application.application import Application
 from bokeh.application.handlers.handler import Handler
 from bokeh.events import Tap
 from bokeh.layouts import column, row
-from bokeh.models import Slider, TextInput
+from bokeh.models import ColumnDataSource, Slider, TextInput, DataTable, TableColumn
 from bokeh.server.server import Server
 from bokeh.plotting import curdoc
+from bokeh.util import logconfig
 from gnuradio import gr, blocks
 import bokehgui
+import functools
 import numpy as np
+import logging
+import logging.handlers
 import socket
 import rci.client
 import run
 from bokeh_models import Skymap
 
+class LogWatcher(logging.handlers.QueueHandler):
+    def __init__(self, doc, cds):
+        super().__init__(cds)
+        self.setLevel(logging.INFO)
+        self.doc = doc
+        self.rollover = 100
+
+    def filter(self, record):
+        if not super().filter(record):
+            return False
+        if record.name in (
+                'bokeh.server.views.ws',
+                'tornado.access',
+        ):
+            return False
+        return True
+
+    def enqueue(self, record):
+        self.doc.add_next_tick_callback(
+            functools.partial(
+                self.queue.stream,
+                {
+                    "asctime": [record.asctime],
+                    "levelname": [record.levelname],
+                    "message": [record.message],
+                },
+                rollover=self.rollover,
+            )
+        )
+
 class SessionHandler(Handler):
     def __init__(self):
         super().__init__()
-        self.sinks = {}
 
     def on_server_loaded(self, server_context):
         self.client = rci.client.Client(client_name='gal_scan')
@@ -37,23 +70,35 @@ class SessionHandler(Handler):
         self.tb.wait()
 
     async def on_session_created(self, session_context):
-        self.tb.lock()
-        self.sinks[session_context.id] = bokehgui.vec_sink_f_proc(self.tb.get_num_channels(), "", 1)
-        self.tb.connect((self.mag_to_aW, 0), (self.sinks[session_context.id], 0))
-        self.tb.unlock()
+        session_context.test_variable = 123
 
     async def on_session_destroyed(self, session_context):
-        self.tb.lock()
-        self.tb.disconnect((self.mag_to_aW, 0), (self.sinks[session_context.id], 0))
-        del self.sinks[session_context.id]
-        self.tb.unlock()
+        logging.info("Session %s destroyed", session_context.id)
+        logging.debug("test_variable = %s", getattr(session_context, 'test_variable'))
 
     def modify_document(self, doc):
+        self.tb.lock()
+        sink = bokehgui.vec_sink_f_proc(self.tb.get_num_channels(), "", 1)
+        self.tb.connect((self.mag_to_aW, 0), (sink, 0))
+        self.tb.unlock()
+
+        # TODO: Populate with a snapshot of log messages
+        log_cds = ColumnDataSource(data=dict(asctime=[''], levelname=[''], message=['']))
+        lw = LogWatcher(doc, log_cds)
+        logging.getLogger().addHandler(lw)
+
+        def cleanup_session(session_context):
+            self.tb.lock()
+            self.tb.disconnect((self.mag_to_aW, 0), (sink, 0))
+            self.tb.unlock()
+            logging.getLogger().removeHandler(lw)
+        doc.on_session_destroyed(cleanup_session)
+
         doc.title = "Gal Scan GUI"
 
         plot_lst = []
 
-        plot = bokehgui.vec_sink_f(doc, plot_lst, self.sinks[doc.session_context.id], is_message =False)
+        plot = bokehgui.vec_sink_f(doc, plot_lst, sink, is_message =False)
 
         plot.initialize(update_time = 100, legend_list = [''])
         plot.get_figure().aspect_ratio = 2
@@ -69,11 +114,26 @@ class SessionHandler(Handler):
         gain = Slider(title="gain", value=self.tb.get_sdr_gain(), start=0, end=65)
         gain.on_change('value', lambda name, old, new: self.tb.set_sdr_gain(new))
 
-        controls = column(gain)
+        # TODO: Automatically sort by asctime descending
+        # TODO: Autoscale columns
+        log_table = DataTable(
+            source=log_cds,
+            columns=[
+                TableColumn(field="asctime", title="Time"),
+                TableColumn(field="levelname", title="Level"),
+                TableColumn(field="message", title="Message"),
+            ],
+            aspect_ratio=2,
+            sizing_mode="stretch_width",
+            sortable=True,
+        )
+
+        controls = column(gain, log_table)
 
         skymap = Skymap(height=600, sizing_mode="stretch_height")
         def on_tap(event):
             # TODO: Dispatch self.tb.point on the execution thread instead
+            logging.info("Manually slewing to (%.2f, %.2f)", event.x, event.y)
             self.client.set_azimuth_position(event.x)
             self.client.set_elevation_position(event.y)
         skymap.on_event(Tap, on_tap)
@@ -100,6 +160,10 @@ class SessionHandler(Handler):
         doc.add_periodic_callback(update_status, 200)
 
 if __name__ == '__main__':
+    logconfig.basicConfig(
+        format="%(asctime)-15s %(levelname)-8s [%(name)s] [%(module)s:%(funcName)s] %(message)s",
+        level=logging.DEBUG,
+    )
     server = Server(
         {'/': Application(SessionHandler())},
         allow_websocket_origin=[socket.getfqdn().lower()+":5006"],
